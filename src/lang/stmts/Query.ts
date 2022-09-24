f0ac83d2f3a305453c293dc3b8c52907c47b2bc1
Xie Yuheng <xyheme@gmail.com>
2022-09-24 20:33:57 +0800

    up

--- src/lang/stmts/Query.ts
+++ src/lang/stmts/Query.ts
@@ -3,24 +3,11 @@ import { Mod } from "../mod"
 import { Span } from "../span"
 import { Stmt } from "../stmt"
 import { Solver } from "../solver"
-
-export type QueryBinding = QueryBindingName
-
-export type QueryBindingName = {
-  kind: "QueryBindingName"
-  name: string
-}
-
-export function QueryBindingName(name: string): QueryBindingName {
-  return {
-    kind: "QueryBindingName",
-    name,
-  }
-}
+import { formatSolution } from "../solution"
 
 export class Query extends Stmt {
   constructor(
-    public bindings: Array<QueryBinding>,
+    public names: Array<string>,
     public goals: Array<Goal>,
     public span?: Span,
   ) {
@@ -31,7 +18,7 @@ export class Query extends Stmt {
     const solver = Solver.forGoals(this.goals)
     const solutions = solver.solve(mod.env)
     for (const solution of solutions) {
-      console.log(solution)
+      console.log(formatSolution(solution, this.names))
     }
   }
 }
