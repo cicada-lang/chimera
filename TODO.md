9c4dca8cbf38676b4d3a5b9d3fb9658606b6de24
Xie Yuheng <xyheme@gmail.com>
2022-10-11 02:43:01 +0800

    MarkdownScript

--- TODO.md
+++ TODO.md
@@ -1,4 +1,3 @@
-MarkdownScript
 createScript
 
 # learn
---
+++ src/scripts/MarkdownScript.ts
@@ -0,0 +1,59 @@
+import * as commonmark from "commonmark"
+import { Mod } from "../lang/mod"
+import { parseStmts } from "../lang/parse"
+import { Script } from "../script"
+
+export class MarkdownScript extends Script {
+  constructor(public mod: Mod, public text: string) {
+    super()
+  }
+
+  async run(): Promise<void> {
+    for (const block of collectBlocks(this.text)) {
+      await this.runBlock(block)
+    }
+  }
+
+  buildText(block: Block): string | undefined {
+    if (block.info === "cicada") {
+      return block.code
+    }
+
+    if (block.info.startsWith("cicada") && (block.info + " ").includes(" compute ")) {
+      return "compute " + block.code
+    }
+  }
+
+  async runBlock(block: Block): Promise<void> {
+    const text = this.buildText(block)
+    if (text === undefined) return
+
+    const stmts = parseStmts(text)
+    await this.mod.executeStmts(stmts)
+  }
+}
+
+type Block = { index: number; info: string; code: string }
+
+function collectBlocks(text: string): Array<Block> {
+  const reader = new commonmark.Parser()
+  const parsed: commonmark.Node = reader.parse(text)
+  const blocks = []
+  const walker = parsed.walker()
+  let counter = 0
+  let event, node
+  while ((event = walker.next())) {
+    node = event.node
+    if (event.entering && node.type === "code_block") {
+      const [start_pos, _end_pos] = node.sourcepos
+      const [row, col] = start_pos
+      blocks.push({
+        index: counter++,
+        info: node.info || "",
+        code: node.literal || "",
+      })
+    }
+  }
+
+  return blocks
+}
