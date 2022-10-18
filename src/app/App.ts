bb16c109ebc1b7142b5b925517993373bf7b640c
Xie Yuheng <xyheme@gmail.com>
2022-10-18 10:16:35 +0800

    up

--- src/app/App.ts
+++ src/app/App.ts
@@ -1,10 +1,7 @@
 import * as Loggers from "../framework/loggers"
 import { AppConfig } from "./AppConfig"
-import { AppHome } from "./AppHome"
 
 export class App {
   logger = new Loggers.PrettyLogger()
-
   config = new AppConfig()
-  home = new AppHome()
 }
