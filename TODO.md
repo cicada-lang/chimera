7aedf72b7c2ebc63832a2ca23e0057ce08b46c52
Xie Yuheng <xyheme@gmail.com>
2023-01-01 21:20:00 +0800

    `Private` -- implement `*Sync` method

--- TODO.md
+++ TODO.md
@@ -1,7 +1,5 @@
 # functional
 
-`Private` -- implement `*Sync` method
-
 `ImportAll` `Import` -- implement `*Sync` method for error report
 
 `Mod.executeStmtsSync` -- without `initialize` (must already `initialized`)
--- src/lang/stmts/Private.ts
+++ src/lang/stmts/Private.ts
@@ -13,11 +13,27 @@ export class Private extends Stmt {
     }
   }
 
+  prepareSync(mod: Mod): void {
+    for (const stmt of this.stmts) {
+      stmt.prepareSync(mod)
+    }
+  }
+
   async execute(mod: Mod): Promise<void> {
     for (const stmt of this.stmts) {
       await stmt.execute(mod)
 
       for (const name of await stmt.boundNames(mod)) {
+        mod.privateNames.add(name)
+      }
+    }
+  }
+
+  executeSync(mod: Mod): void {
+    for (const stmt of this.stmts) {
+      stmt.executeSync(mod)
+
+      for (const name of stmt.boundNamesSync(mod)) {
         mod.privateNames.add(name)
       }
     }
