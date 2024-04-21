75647bc36538c52ed18cf64016b322d96c295dcd
Xie Yuheng <xyheme@gmail.com>
2024-04-21 16:18:17 +0800

    up

---
+++ src/test.ts
@@ -0,0 +1,11 @@
+import { test } from "node:test"
+import assert from "node:assert"
+
+test('snapshot docs/**/*.ch', () => {
+  assert
+})
+
+
+test('snapshot docs/**/*.error.ch', () => {
+  assert
+})
