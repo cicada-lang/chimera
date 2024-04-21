0ddf56296631490ce9a955655794c4a112b5aaf5
Xie Yuheng <xyheme@gmail.com>
2024-04-21 18:31:10 +0800

    尝试用 node:test 而不用 test-runner
    - 因为每次开启 node 解释器有严重的 overhead。

--- TODO.md
+++ TODO.md
@@ -1,7 +1,3 @@
-尝试用 node:test 而不用 test-runner
-
-- 因为每次开启 node 解释器有严重的 overhead。
-
 update to esmodule
 
 remove rule support
--- src/test.ts
+++ src/test.ts
@@ -1,11 +1,41 @@
+import { glob } from "glob"
+import fs from "node:fs"
 import { test } from "node:test"
-import assert from "node:assert"
+import { Runner } from "./command-line/Runner"
+import { stripAnsi } from "./utils/ansi"
 
-test('snapshot docs/**/*.ch', () => {
-  assert
+test("snapshot docs/**/*.ch", async () => {
+  const files = await glob(`${__dirname}/../docs/**/*.ch`)
+  for (const file of files) {
+    if (file.endsWith(".error.ch")) {
+      await snapshotError(new URL(`file:${file}`))
+    } else {
+      await snapshot(new URL(`file:${file}`))
+    }
+  }
 })
 
+async function snapshot(url: URL): Promise<void> {
+  console.log(`[snapshot] ${url}`)
+  const runner = new Runner()
+  const mod = runner.loader.load(url)
+  const output = Array.from(mod.outputs.values()).join("\n")
+  if (output) {
+    await fs.promises.writeFile(`${url.pathname}.out`, output + "\n")
+  }
+}
 
-test('snapshot docs/**/*.error.ch', () => {
-  assert
-})
+async function snapshotError(url: URL): Promise<void> {
+  console.log(`[snapshotError] ${url}`)
+  try {
+    const runner = new Runner()
+    runner.loader.load(url)
+    throw new Error(`[snapshotError] Expecting error.`)
+  } catch (error) {
+    if (!(error instanceof Error)) throw error
+    await fs.promises.writeFile(
+      `${url.pathname}.err`,
+      stripAnsi(error.message) + "\n",
+    )
+  }
+}
