1465f35a0990d1e6174e7364f0cac1e5a47f9690
Xie Yuheng <xyheme@gmail.com>
2024-04-21 16:12:37 +0800

    remove AppHome

--- src/app/App.ts
+++ src/app/App.ts
@@ -1,9 +1,7 @@
 import * as Loggers from "@cicada-lang/framework/lib/loggers"
 import { AppConfig } from "./AppConfig"
-import { AppHome } from "./AppHome"
 
 export class App {
   logger = new Loggers.PrettyLogger()
   config = new AppConfig()
-  home = new AppHome()
 }
