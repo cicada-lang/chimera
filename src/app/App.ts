27736b9039e44972e98066bea36fea481cf85d4f
Xie Yuheng <xyheme@gmail.com>
2024-04-24 22:57:38 +0800

    remove app

--- src/app/App.ts
+++
@@ -1,7 +0,0 @@
-import * as Loggers from "@cicada-lang/framework/lib/loggers"
-import { AppConfig } from "./AppConfig.js"
-
-export class App {
-  logger = new Loggers.PrettyLogger()
-  config = new AppConfig()
-}
--- src/app/index.ts
+++
@@ -1,9 +0,0 @@
-import { App } from "./App.js"
-
-declare global {
-  var app: App
-}
-
-export const app = new App()
-
-globalThis.app = app
